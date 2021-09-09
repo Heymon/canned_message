ec1c09f089f2599afde1f140f8f2a6d0aa869bd4
Heymon <rcavalleiro@gmail.com>
2021-09-08 20:38:16 -0700

    refactors public script to keep track of userInfo thru cookie and jwt & adds AuthModel clas

--- public/scripts/messageSocketHandler.js
+++ public/scripts/messageSocketHandler.js
@@ -17,8 +17,22 @@
             return fetch('https://randommer.io/api/Name?nameType=firstname&quantity=1',{
                 method: "GET",
                 headers: {
-                    "X-Api-Key": "06740e2146ad4bfdaa9bc239ea906140",
+                    "X-Api-Key": "06740e2146ad4bfdaa9bc239ea906140",// needs to be reseted because it is already saved in git
                 },
+            }).then(response => response.json());
+        }
+    }
+
+    const baseURL = "http://localhost:3000";
+
+    class AuthModel {
+        static register = data => {
+            return fetch (`${baseURL}/register`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data)
             }).then(response => response.json());
         }
     }
@@ -54,32 +68,50 @@
         } 
     });
 
-    socket.on('user.connected', (db) =>{//once connection is received from server
+    socket.on('user.connected', (id) =>{//once connection is received from server
+        console.log(id);
         //it sets user name
         ApiRequest.requestColor().then(json => { //by fetching random color from API
             console.log(json.name.value);
-            let color = json.name.value;
-            console.log(color);
+            let colorName = json.name.value;
+            let colorHex = json.hex.value;
             ApiRequest.requestName().then(json => { //by fetching random name from API
                 console.log(json[0]);
                 let name = json[0];
-                let userName = `${color} ${name}`   //putting both fetches together
-                // sets name on page
-                var item = document.createElement('li');
-                item.textContent = `You are connected as ${userName}`;
+                let userName = `${colorName} ${name}`   //putting both fetches together
+                let userInfo = {hexCode : colorHex, userName : userName}
                 // TODO here it needs to save into jwt info like; socket.id, userName, color hexcode
-                messages.appendChild(item);
-                window.scrollTo(0, document.body.scrollHeight);//keeps page at the bottom
-                socket.emit('new user', userName); //sends message to server that name has been establish
+                localStorage.setItem("uif", JSON.stringify(userInfo));//saves a cookie with accessible info
+                AuthModel.register({socketId : id.socketId, ...userInfo}).then( json => {
+                    console.log(json);
+                    localStorage.setItem("uid", JSON.stringify(json.signedJwt));//sets the jwt
+                    console.log(JSON.parse(localStorage.uif));       
+                    
+                    // sets name on page
+                    let item = document.createElement('li');
+                    let subItem = document.createElement('span');
+                    subItem.textContent = `${userName}`;
+                    subItem.style.color = colorHex;
+                    item.textContent = 'You are connected as ';
+                    item.appendChild(subItem);
+                    messages.appendChild(item);
+                    window.scrollTo(0, document.body.scrollHeight);//keeps page at the bottom
+                    socket.emit('new user', userInfo); //sends message to server that name has been establish
+                })
             })
 
         })
     })
 
-    socket.on('new user', function (userName) {
-        console.log(userName);
-        var item = document.createElement('li');
-        item.textContent = `${userName} has connected`;
+    socket.on('new user', function (userInfo) {
+        console.log(userInfo);
+        //sends hex color and user name to other sockets
+        let item = document.createElement('li');
+        let subItem = document.createElement('span');
+        subItem.textContent = `${userInfo.userName}`;
+        subItem.style.color = userInfo.hexCode;
+        item.textContent = ' has connected';
+        item.prepend(subItem);
         messages.appendChild(item);
         window.scrollTo(0, document.body.scrollHeight);//keeps page at the bottom
     })
