bcb909e60fda661cb3451a016b590229e70692b6
Heymon <rcavalleiro@gmail.com>
2021-09-28 22:44:20 -0700

    adds class for user messages & configurations to keep name color contrast ratio above 4.5

--- public/scripts/messageSocketHandler.js
+++ public/scripts/messageSocketHandler.js
@@ -4,7 +4,17 @@
 
     class ApiRequest {
         static requestColor = () => {
-            let hex = getRandomHex();
+            let hex = "";
+            let isContrastGood = false;
+            while (!isContrastGood) {
+                hex = getRandomHex();
+                let contrast = contrastRatio(hexToRgb(hex), [250,235,215]);
+                let contrast2 = contrastRatio(hexToRgb(hex), [255,218,168]);
+                if (contrast > 4.5 && contrast2 > 4.5) {
+                    isContrastGood = true
+                }
+                
+            }
             return fetch(`https://www.thecolorapi.com/id?hex=${hex}&format=json`,{
                 method: "GET",
                 headers: {
@@ -50,13 +60,57 @@
         return color;
     }
 
-    const addMessage = (msg, userInfo, isPrepend) => {
+    /** 
+     * Copied/modified from https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+     * 
+     */
+    function hexToRgb(hex) {
+        var bigint = parseInt(hex, 16);
+        var r = (bigint >> 16) & 255;
+        var g = (bigint >> 8) & 255;
+        var b = bigint & 255;
+    
+        return [r, g, b];
+    }
+
+    const contrastRatio = (colorOne, colorTwo) =>{//lighter color(closest to 1) divided by dacker(closest to 0) color according to https://www.accessibility-developer-guide.com/knowledge/colours-and-contrast/how-to-calculate/
+
+        let l1 = relativeLuminance(colorOne);
+        let l2 = relativeLuminance(colorTwo);
+
+        if (l1 < l2) {
+            return (l2+0.05)/(l1+0.05)           
+        }else {
+            return (l1+0.05)/(l2+0.05)
+        }
+        
+    }
+
+    const relativeLuminance = (color) =>{//formula from https://www.w3.org/WAI/GL/wiki/Relative_luminance
+
+        let realRGB = [];
+        
+        for (let i = 0; i < 3; i++) {
+            let sRGB = color[i]/255;
+            if (sRGB <= 0.04045) {
+                realRGB.push(sRGB/12.92);
+            }else {
+                realRGB.push(Math.pow((sRGB+0.055)/1.055, 2.4));
+            }
+        }
+        return (0.2126*realRGB[0])+(0.7152*realRGB[1])+(0.0722*realRGB[2])
+
+    }
+
+    const addMessage = (msg, userInfo, isUser, isPrepend) => {
         // sets name on page
         let item = document.createElement('li');
         let subItem = document.createElement('span');
         subItem.textContent = `${userInfo.userName}`;
         subItem.style.color = userInfo.colorHex;
         item.textContent = msg;
+
+        if(isUser)item.setAttribute("class", "user--message");
 
         if (!isPrepend) {
             item.appendChild(subItem);
@@ -65,7 +119,7 @@
             item.prepend(subItem);
             messages.appendChild(item);
         } 
-        window.scrollTo(0, document.body.scrollHeight);//keeps page at the bottom
+        messages.scrollTo(0, messages.scrollHeight);//keeps page at the bottom
     }
 
     const updateUsersList = (userInfo, isConnecting) => {
@@ -128,7 +182,7 @@
         e.preventDefault();
         if(input.value){
             if(localStorage.uif){
-                let userMsg = {msg: input.value+" -", userInfo:{...JSON.parse(localStorage.uif)}};
+                let userMsg = {msg: input.value, userInfo:{...JSON.parse(localStorage.uif)}};
                 console.log(userMsg);
                 socket.emit('chat message', userMsg);
                 input.value = '';
@@ -144,7 +198,6 @@
             updateUsersList({userName: curValue.userName, colorHex: curValue.colorHex}, true);
         });
 
-        // TODO create a function to calculate the contrast ratio through relative luminace of both the user color & the background color
         // TODO create function to check if the name is a duplicate
         //it sets user name
         ApiRequest.requestColor().then(json => { //by fetching random color from API
@@ -174,17 +227,17 @@
 
     socket.on('user.disconnected', function (userInfo) {
         updateUsersList(userInfo, false);
-        addMessage(' has disconnected', userInfo, true);  
+        addMessage(' has disconnected', userInfo, false, true);  
     })
 
     socket.on('new user', function (userInfo) {
         // console.log(userInfo);
         //display on DOM new user connected
-        addMessage(' has connected', userInfo, true);
+        addMessage(' has connected', userInfo, false, true);
         updateUsersList(userInfo, true);
     })
 
     socket.on('chat message', function (userMsg) {
         //adds new message to DOM
-        addMessage(userMsg.msg, userMsg.userInfo, false)
+        addMessage(userMsg.msg, userMsg.userInfo, true, false)
     })
